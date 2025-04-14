3c65c6d4dd432641670f7e47d9c6b5401e8ebbbe
jacobyc <jacobyc@spotv.net>
2025-04-14 13:40:44 +0900

    [Fix] supabase 관련 로직 수정

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,8 +1,11 @@
 // supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || process.env.SUPABASE_URL; // .env 파일에서 URL 가져오기
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY; // .env 파일에서 Key 가져오기
+const supabaseUrl = (typeof import.meta !== 'undefined' && import.meta.env.VITE_SUPABASE_URL) || process.env.SUPABASE_URL;
+const supabaseAnonKey = (typeof import.meta !== 'undefined' && import.meta.env.VITE_SUPABASE_ANON_KEY) || process.env.SUPABASE_ANON_KEY;
+
+// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || process.env.SUPABASE_URL; // .env 파일에서 URL 가져오기
+// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY; // .env 파일에서 Key 가져오기
 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
